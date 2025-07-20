088387955d05b160e855a8297d9d4ac2346ecfcd
Lukas Gonzales <lukas@soundhard.com>
2025-07-20 10:32:43 -1000

    .

--- src/app/(frontend)/search/page.tsx
+++ src/app/(frontend)/search/page.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from 'next/types'
+import type { Metadata } from "next/types";
 
-import { CollectionArchive } from '@/components/CollectionArchive'
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-import React from 'react'
-import { Post } from '@/payload-types'
-import { Search } from '@/search/Component'
-import PageClient from './page.client'
-import { CardPostData } from '@/components/Card'
+import { CollectionArchive } from "@/components/CollectionArchive";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+import React from "react";
+import { Search } from "@/search/Component";
+import PageClient from "./page.client";
+import { CardPostData } from "@/components/Card";
 
 type Args = {
   searchParams: Promise<{
-    q: string
-  }>
-}
-export default async function Page({ searchParams: searchParamsPromise }: Args) {
-  const { q: query } = await searchParamsPromise
-  const payload = await getPayload({ config: configPromise })
+    q: string;
+  }>;
+};
+export default async function Page({
+  searchParams: searchParamsPromise,
+}: Args) {
+  const { q: query } = await searchParamsPromise;
+  const payload = await getPayload({ config: configPromise });
 
   const posts = await payload.find({
-    collection: 'search',
+    collection: "search",
     depth: 1,
     limit: 12,
     select: {
@@ -40,12 +41,12 @@ export default async function Page({ searchParams: searchParamsPromise }: Args) 
                 },
               },
               {
-                'meta.description': {
+                "meta.description": {
                   like: query,
                 },
               },
               {
-                'meta.title': {
+                "meta.title": {
                   like: query,
                 },
               },
@@ -58,7 +59,7 @@ export default async function Page({ searchParams: searchParamsPromise }: Args) 
           },
         }
       : {}),
-  })
+  });
 
   return (
     <div className="pt-24 pb-24">
@@ -73,17 +74,17 @@ export default async function Page({ searchParams: searchParamsPromise }: Args) 
         </div>
       </div>
 
-      {posts.totalDocs > 0 ? (
+      {/* {posts.totalDocs > 0 ? (
         <CollectionArchive posts={posts.docs as CardPostData[]} />
       ) : (
         <div className="container">No results found.</div>
-      )}
+      )} */}
     </div>
-  )
+  );
 }
 
 export function generateMetadata(): Metadata {
   return {
     title: `Payload Website Template Search`,
-  }
+  };
 }
