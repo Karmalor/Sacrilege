7c0dcf98af86af27f2af339f6950b8c3eb9277a2
Lukas Gonzales <lukas@soundhard.com>
2025-07-20 16:44:35 -1000

    .

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -72,7 +72,7 @@ export async function POST(request: Request) {
       }
     );
 
-    console.log("poo");
+    console.log("productDataObject", productDataObject);
 
     lineItems.data.map(async (lineItem) => {
       for (let i = 0; i < (lineItem.quantity || 1); i++) {
