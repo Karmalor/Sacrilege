c69e2a87acd5ca78c77668b445b30fede0e553ab
Lukas Gonzales <lukas@soundhard.com>
2025-08-10 00:14:46 -0500

    Adding FaceBook pixel for marketing

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -20,6 +20,9 @@ import { getUser } from "./(authenticated)/_actions/getUser";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
 export default async function RootLayout({
   children,
 }: {
@@ -27,6 +30,10 @@ export default async function RootLayout({
 }) {
   const user = await getUser();
   const { isEnabled } = await draftMode();
+
+  const PixelTracker = dynamic(
+    () => import("src/components/Marketing/PixelTracker")
+  );
 
   return (
     <html
@@ -38,6 +45,36 @@ export default async function RootLayout({
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                         
+              n.callMethod.apply(n,arguments):n.queue.push   
+              (arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!
+              0;n.version='2.0';n.queue=[];t=b.createElement(e);
+              t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'YOUR_PIXEL_ID');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            alt="."
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=
+            PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         <Providers>
