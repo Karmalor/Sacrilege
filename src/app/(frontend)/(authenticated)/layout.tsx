dd9c48c6cf7b8645758b42a64de58b66ca95d1b5
Lukas Gonzales <lukas@soundhard.com>
2025-08-23 10:09:37 -0500

    .

--- src/app/(frontend)/(authenticated)/layout.tsx
+++ src/app/(frontend)/(authenticated)/layout.tsx
@@ -1,13 +1,14 @@
 import { redirect } from "next/navigation";
 import React, { FC, ReactNode } from "react";
 import { getMeUser } from "@/utilities/getMeUser";
+import { getUser } from "./_actions/getUser";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = async ({ children }) => {
-  const { user } = await getMeUser();
+  const user = await getUser();
 
   console.log(user);
 
